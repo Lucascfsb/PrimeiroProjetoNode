b95b69759000c0de625dd6c6fa7ccb73610d51f6
Lucascfsb <lucascfsb@gmail.com>
2021-10-07 20:32:42 -0300

    Frist Commit
    Basic configuration and start of the project and classes

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ message: 'Hello GoStack' }));
+
+app.listen(3333, () => {
+  console.log('🥨 Server started on port 3333');
+});
