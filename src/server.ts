09d54c96f8706d5bd0390854a22a0c84f429b6f3
Lucascfsb <lucascfsb@gmail.com>
2021-10-07 21:28:42 -0300

    Debug Class

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'Hello GoStack' }));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🥨 Server started on port 3333');
+  console.log('🍟 Server started on port 3333!');
 });
